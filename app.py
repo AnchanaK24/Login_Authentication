f6393ac43c3387b8b10179796aad7989318d83fa
AnchanaK24 <74914989+AnchanaK24@users.noreply.github.com>
2021-06-13 22:20:01 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,46 +1,12 @@
-import json
-from flask import Flask, request,make_response
-from flask_sqlalchemy import  SQLAlchemy
 
-app=Flask(__name__)
+from init import init_app
 
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///todo.db'
-
-db = SQLAlchemy(app)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(80),unique=True,nullable=True)
-    name=db.Column(db.String(50))
-    password=db.Column(db.String(80))
+app=init_app()
 
 
-class Todo(db.Model):
-    text = db.Column(db.String(50))
-    user_email=db.Column(db.String(50),primary_key=True)
-
-@app.route('/user/<email>',methods=['GET'])
-def get_user(email):
-    email=request.form.get(email)
-    user=User.query.filter_by(email=email).first()
-
-    if  not user:
-        return make_response({'Message':'User not found'})
-
-    data = request.get_json()
-    name=data['name']
-    password=data['password']
-    return make_response({'name':name,'password':password})
-
-@app.route('/user',methods=['POST'])
-def create_user():
-    data=request.get_json()
-    password =data['password']
-    email=data['email']
-    new_user=data['name']
-
-    return make_response({'message':'New User Created'})
-
+@app.route('/')
+def welcome():
+    return "Welcome"
 
 if __name__ ==  '__main__':
     app.run(debug=True)
